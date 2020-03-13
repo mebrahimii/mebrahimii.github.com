807358ca2a126d25ba361e50f1f90814b168d7e3
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2020-03-12 19:59:54 -0700

    1

--- publications/js/menucollapse.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-             $("button.navbar-toggle").click();
-        }
-    });
-});

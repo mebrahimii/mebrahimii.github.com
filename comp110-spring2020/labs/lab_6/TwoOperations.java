735b0d45f9a1c1de0c9d341e8d815d5f20133046
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2020-03-14 23:03:24 -0700

    lab 6

---
+++ comp110-spring2020/labs/lab_6/TwoOperations.java
@@ -0,0 +1,31 @@
+public class TwoOperations {
+    // You must define three static methods:
+    //
+    // 1.) A method named multDiv, which takes two doubles
+    //     (hereafter named `a` and `b`) and returns an instance
+    //     of TwoDoubles.  The first instance variable of the
+    //     returned TwoDoubles instance must hold the result of
+    //     a * b, and the second instance variable of the returned
+    //     TwoDoubles instance must hold the result of a / b.
+    //
+    // 2.) A method named subAdd, which takes two doubles
+    //     (hereafter named `a` and `b`) and returns an instance
+    //     of TwoDoubles.  The first instance variable of the
+    //     returned TwoDoubles instance must hold the result of
+    //     a - b, and the second instance variable of the returned
+    //     TwoDoubles instance must hold the result of a + b.    
+    //
+    // 3.) A main method, which will do the following, in order:
+    //     1.) Prompt the user with the String: "Enter first double: "
+    //     2.) Read in a double from the user, hereafter named `a`
+    //     3.) Prompt the user with the String: "Enter second double: "
+    //     4.) Read in a double from the user, hereafter named `b`
+    //     5.) Call multDiv with `a` and `b`, respectively
+    //     6.) Call the printMe method on the TwoDoubles object returned
+    //         by multDiv
+    //     7.) Call subAdd with `a` and `b`, respectively
+    //     8.) Call the printMe method on the TwoDoubles object returned
+    //         by subAdd
+    //     
+    // TODO - write your code below this comment.
+}

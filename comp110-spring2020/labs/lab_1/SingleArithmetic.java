b818cec71d5ddbf7ea2802a74ed6ac93f77f15c5
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2020-03-14 20:41:30 -0700

    add more labs

---
+++ comp110-spring2020/labs/lab_1/SingleArithmetic.java
@@ -0,0 +1,7 @@
+public class SingleArithmetic {
+    public static void main(String[] args) {
+        // The `/` operator indicates division
+        // Java uses PEMDAS ordering for arithmetic expressions
+        System.out.println(1 + 2 * 7 / (1 + 1));
+    }
+}
---
+++ comp110-spring2020/labs/lab_2/MultiPrint.java
@@ -0,0 +1,9 @@
+public class MultiPrint {
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        // TODO: make this also print "third", "fourth", and "fifth"
+        // in the same manner as above.  You will need three statements.
+        // ---WRITE YOUR CODE BELOW THIS COMMENT---
+    }
+}

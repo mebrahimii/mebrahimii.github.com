e82deb45dafe4e40b935a6316c5873086ab03c1e
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2020-03-14 21:04:21 -0700

    1

--- comp110-spring2020/labs/lab_2/SingleArithmetic.java
+++
@@ -1,7 +0,0 @@
-public class SingleArithmetic {
-    public static void main(String[] args) {
-        // The `/` operator indicates division
-        // Java uses PEMDAS ordering for arithmetic expressions
-        System.out.println(1 + 2 * 7 / (1 + 1));
-    }
-}

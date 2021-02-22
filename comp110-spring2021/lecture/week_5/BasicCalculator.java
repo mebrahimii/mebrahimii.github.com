139ed19b35bab2baa8b988962ed768da55e25cbb
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2021-02-22 13:37:29 -0800

    1

---
+++ comp110-spring2021/lecture/week_5/BasicCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+   int number1;
+   int number2;
+
+   public BasicCalculator(int n1, int n2) {
+      number1 = n1;
+      number2 = n2;
+   }
+
+   public int addition() {
+      return number1 + number2;
+   }
+
+   public int substraction() {
+      return number1 - number2;
+   }
+
+   public int multiplication() {
+      return number1 * number2;
+   }
+
+   public int division() {
+      return number1 / number2;
+   }
+
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Insert the first integer number: ");
+      int num1 = input.nextInt();
+      System.out.print("Insert the second integer number: ");
+      int num2 = input.nextInt();
+      
+      BasicCalculator bc = new BasicCalculator(num1, num2);
+      int addRes = bc.addition();
+      int subRes = bc.substraction();
+      int mulRes = bc.multiplication();
+      double divRes = bc.division();
+      
+      System.out.println("The addition is: " + addRes);
+      System.out.println("The substraction is: " + subRes);
+      System.out.println("The multiplication is: " + mulRes);
+      System.out.println("The division is: " + divRes); 
+
+   }
+}

32fdf0abae8bbcbcf519d675df245bafa3d1dbb0
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2021-02-22 13:50:09 -0800

    1

--- comp110-spring2021/lecture/week_5/BasicCalculatorPP.java
+++ comp110-spring2021/lecture/week_5/BasicCalculatorPP.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
-public class BasicCalculator {
+public class BasicCalculatorPP {
+
+   public static int addition(int n1, int n2) {
+      return n1 + n2;
+   }
+
+   public static int substraction(int n1, int n2) {
+      return n1 - n2;
+   }
+
+   public static int multiplication(int n1, int n2) {
+      return n1 * n2;
+   }
+
+   public static double division(int n1, int n2) {
+      return (double) n1 / n2;
+   }
 
    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
@@ -9,10 +25,10 @@ public class BasicCalculator {
       System.out.print("Insert the second integer number: ");
       int num2 = input.nextInt();
       
-      int addRes = num1 + num2;
-      int subRes = num1 - num2;
-      int mulRes = num1 * num2;
-      double divRes = (double) num1 / num2;
+      int addRes = addition(num1, num2);
+      int subRes = substraction(num1, num2);
+      int mulRes = multiplication(num1, num2);
+      double divRes = division(num1, num2);
       
       System.out.println("The addition is: " + addRes);
       System.out.println("The substraction is: " + subRes);

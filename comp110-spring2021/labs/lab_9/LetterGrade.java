8f2d563303df2c73d3e61607c96cd87709f1d7f0
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2021-03-08 06:31:22 -0800

    Week 7

--- comp110-spring2021/labs/lab_9/LetterGrade.java
+++ comp110-spring2021/labs/lab_9/LetterGrade.java
@@ -4,8 +4,8 @@ public class LetterGrade {
     // The method you write will return a String representing a letter
     // grade (e.g., "A", "A-", "B+", etc.).
     // The letter grade is determined by the given percentage, according
-    // to the scale specified on page 2 of the class syllabus (available
-    // here: https://kyledewey.github.io/comp110-spring18/syllabus.pdf).
+    // to the scale specified on page 4 of the class syllabus (available
+    // here: https://mebrahimii.github.io/comp110-spring2021/syllabus.pdf).
     // You may assume that the given percentage is between 0.0 and 100.0
     // TODO - write your code below this comment.
     
--- comp110-spring2021/labs/lab_9/LetterGradeTest.java
+++ comp110-spring2021/labs/lab_9/LetterGradeTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class LetterGradeTest {
     // TODO - write your code below this comment.
     // Write 12 tests, where each test corresponds
-    // to one of the possible class grades.  As a hint,
+    // to one of the possible class grades. As a hint,
     // most of these tests look a lot like each other, so
     // you might be able to work faster by copy-pasting
     // the same test and then editing it for the given case.

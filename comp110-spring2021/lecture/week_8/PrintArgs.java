90e34035adb097410cbe512c3c9c3cb3f73b0e98
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2021-03-22 08:07:52 -0700

    week 9

--- comp110-spring2021/lecture/week_8/PrintArgs.java
+++
@@ -1,7 +0,0 @@
-public class PrintArgs {
-    public static void main(String[] args) {
-        for (int x = 0; x < args.length; x++) {
-            System.out.println(args[x]);
-        }
-    }
-}

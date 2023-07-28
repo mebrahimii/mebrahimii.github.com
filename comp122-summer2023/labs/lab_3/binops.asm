56a3e8dee9221894ad56c4353a37a74488735055
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2023-07-27 17:45:35 -0700

    Create binops.asm

---
+++ comp122-summer2023/labs/lab_3/binops.asm
@@ -0,0 +1,22 @@
+.text   
+main:
+        # TODO: write your code below this comment
+        # Your code should do the following, in this order:
+        # - Read the first integer from the user, putting it in
+        #   some known register
+        # - Read the second integer from the user, putting it in
+        #   some known register
+        # - Add the two integers, print the result, print a newline
+        # - Subtract the second integer from the first integer,
+        #   print the result, print a newline
+        # - Bitwise AND the two integers together, print the result,
+        #   print a newline
+        # - Bitwise OR the two integers together, print the result,
+        #   print a newline
+        # - Bitwise XOR the two integers together, print the result,
+        #   print a newline
+
+        # exit program
+        li $v0, 10
+        syscall
+        

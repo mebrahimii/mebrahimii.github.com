c0c04e570974eecfafc94f47ad822de92a0dd10c
Mahdi Ebrahimi <48655504+mebrahimii@users.noreply.github.com>
2026-07-19 10:49:10 -0700

    Update slide_config.js

--- comp644-fall2026/slides/15etl/slide_config.js
+++ comp644-fall2026/slides/15etl/slide_config.js
@@ -2,7 +2,7 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Extraction, Transformation, and Loading',
-    subtitle: '<br>Dr. Mahdi EBrahimi',
+    subtitle: '<br>Dr. Mahdi Ebrahimi',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
